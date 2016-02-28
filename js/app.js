d85d13501c6eec489d7567433becfcde2ff72bcb
otterry53 <twlow88@gmail.com>
2016-02-28 11:51:52 -0500

    added items to both cats

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
+//target .input-form or #addItemValue?
+$("input-form").mousenter {
 
-$(document).ready() {
+
+})
+.mouseleave {
 
 
-
+})
+.mousedown {
 
 }
 
 
+
+}
+//
+
 .val()
-.prepend()
-.append()
+    .prepend()
+    .append()
+    .hover()
 
 $(":input")
 $(":text")
 $(":checkbox")
 $(":checked")
 
-$('ul').on('click', 'li', function(event){
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
+$('ul').on('click', 'li', function (event) {
+    // fires when any LIs are clicked on
+    // including LIs that aren't on the page when it is initially loaded
 });
