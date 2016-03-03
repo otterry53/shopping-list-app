0e66e97ba1428bd81d99413443545dafcd059f95
otterry53 <twlow88@gmail.com>
2016-03-02 22:59:11 -0500

    bootstrap problems

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
 $(document).ready(function () {
-    $('.header-container .input-form').
+
+    $('.input-form').hover(function () {
+            $(this).css('outline-color', 'red');
+        }):
 
 
-})
 
 
+        //how do I tie it into dropdown menu?//
 
-
-}
-//
-
-.val()
-    .prepend()
-    .append()
-    .hover()
-
-$(":input")
-$(":text")
-$(":checkbox")
-$(":checked")
-
-$('ul').on('click', 'li', function (event) {
-    // fires when any LIs are clicked on
-    // including LIs that aren't on the page when it is initially loaded
+        var $toAdd = $('#addItemValue');
+    $('.header-container .input-form').keydown(function (key) {
+        if (key.keyCode == 13) {
+            $('.produce .list-column').append('toAdd');
+        }
+    });
 });
+
+
+
+
+
+//Code for changing text decoration when box is clicked on.//
+
+$('.checkbox').click(function)();
+$('li').addClass('.checked');
+});
+
+
+
+
+
+
+
+//Code for removing an item when delete X is clicked on - how do I target that particular li//
+$('.list-column .delete').mousedown(function () {
+    $('.li').remove();
+});
+
+
+
+//Code for removing all//
+$('.delete-all').mousedown(function () {
+    $('.li').remove();
+
+});
+
+
+//.val()//
